f8e66754b36ff9db336451c374cca83e4cf59d27
Zach <egger0a6@email.arizona.edu>
2020-10-29 20:49:04 -0700

    add starter code

---
+++ script.js
@@ -0,0 +1,15 @@
+const viewport = querySelector("#viewport");
+let tileX = 16;
+let tileY = 16;
+let size = 16;
+
+function createGrid() {
+    let grid = document.createElement("div");
+    grid.setAttribute("id", "grid-container");
+    grid.setAttribute("sytle")
+
+    let i;
+    for (i = 0; i < size; i++) {
+
+    }
+}
